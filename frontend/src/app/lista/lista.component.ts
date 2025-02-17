55de9363408aa175758e641d166df814421bcdc9
Aluno <aluno@IPIRANGA.FATEC>
2025-02-17 20:02:32 -0300

    Atualização

--- frontend/src/app/lista/lista.component.ts
+++ frontend/src/app/lista/lista.component.ts
@@ -3,31 +3,50 @@ import { Paciente } from '../model/paciente';
 import { PacienteService } from '../service/paciente.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
- 
+
 @Component({
   selector: 'app-lista',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './lista.component.html',
-  styleUrl: './lista.component.css',
+  styleUrls: ['./lista.component.css'],
   providers: [PacienteService]
 })
 export class ListaComponent {
-  mensagem: String = "";
+  mensagem: string = "";
   pacientes: Paciente[] = [];
- 
- 
-constructor(private service: PacienteService){
-  this.listarPacientes();
- 
+
+  constructor(private service: PacienteService) {
+    this.listarPacientes();
   }
- 
-  listarPacientes(){
+
+  listarPacientes() {
     this.service.listar().subscribe({
-      next: (data) =>{this.pacientes = data;},
-      error: (msg) =>{this.mensagem = "ocorreu erro"}
+      next: (data) => {
+        this.pacientes = data;
+      },
+      error: (msg) => {
+        this.mensagem = "Ocorreu um erro ao listar os pacientes.";
+      }
+    });
+  }
+
+
+  editarPaciente(id: number): void {
+    console.log(`Editar paciente com ID: ${id}`);
+
+  }
+
+  excluirPaciente(id: number): void {
+    console.log(`Excluir paciente com ID: ${id}`);
+    this.service.excluir(id).subscribe({
+      next: () => {
+        this.listarPacientes();
+        this.mensagem = "Paciente excluído com sucesso!";
+      },
+      error: (msg) => {
+        this.mensagem = "Erro ao excluir o paciente.";
+      }
     });
   }
 }
- 
- 
